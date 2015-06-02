40f88ce3073629da2945e54611d3b9db43792d5f
Kulikov Denis <cdbiznes@gmail.com>
2015-06-02 14:44:34 +0300

    Chromecast class without implementation

---
+++ src/CastSharp/Chromecast.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace CastSharp
+{
+    public class Chromecast
+    {
+        public static async Task<List<Chromecast>> GetDevices()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task LaunchApplication(string applicationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Load(Url url)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
